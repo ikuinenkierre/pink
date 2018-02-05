8c36c1bcd758d67f5e3a160eaa0d416946da9485
Serafima <ikuinenkierre@gmail.com>
2018-02-05 18:18:12 +0300

    Перемещает скрипт подключения карты из файла html в scripts.js

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -8,5 +8,31 @@ toggleButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   toggleButton.classList.toggle("page-header__toggle--closed");
   nav.classList.toggle("page-header__nav--open");
-})
+});
+
+// Генерация карты
+
+var map;
+initMap = function() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: new google.maps.LatLng(59.936310,  30.321901),
+    mapTypeId: 'roadmap'
+  });
+
+  var iconBase = 'img/';
+  var icon = iconBase + 'map-pin.svg';
+
+  var feature = {
+    position: new google.maps.LatLng(59.936310,  30.321901)
+  };
+
+ // Create markers. 
+  var marker = new google.maps.Marker({
+    position: feature.position,
+    icon: icon,
+    map: map
+  });
+}
+
 
